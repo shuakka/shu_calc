0b27f33fff060e0a9e0e03300f1185f3880b86b9
shuakka <43882437+shuakka@users.noreply.github.com>
2018-10-05 18:19:07 +0530

    Add files via upload

---
+++ OperatorExample.java
@@ -0,0 +1,11 @@
+class OperatorExample{  
+public static void main(String args[]){  
+int x=10;  
+int y,z,a;
+y=10;z=10;a=10;
+System.out.println(x++);//10 (11)  
+System.out.println(++y);//12  
+System.out.println(z--);//12 (11)  
+System.out.println(--a);//10  
+}
+}
